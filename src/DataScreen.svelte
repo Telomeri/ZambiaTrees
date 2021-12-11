c0cd374bb6992d5fe3612cc7bd4101b05e32819a
Lari-Haapaniemi <lari.haapaniemi@gmail.com>
2021-12-11 13:54:17 +0200

    Rough starting point for the data page, trying to figure out how and what to display here

--- src/DataScreen.svelte
+++ src/DataScreen.svelte
@@ -1,18 +1,31 @@
 <script>
 	import Chart from "svelte-frappe-charts";
 	export let data;
+	//show the amount planted per person?
+	//show the amount planted and checked per month? as a heatmap?
+	//show a scatter plot of the coordinates??
+	//show the amount of pictures uploaded? Maybe?
+	let growthAmount = [0,0,0,0];
+	data.forEach((x,i) => growthAmount[x.growth] += 1);
+	let growthData = {
+		labels : ["Seed","Sapling","Mature","Decline"],
+		datasets: [{
+			values: growthAmount
+		}]
+		
+	};
   </script>
-
-<div>
-
+<div class="chartGrowth">
+	<p>Growth state</p>
+	<Chart data={growthData} type='percentage' colors={['#7F675B','#94FBAB','#058C42','#464e3c']} height={150} maxSlices = {5}/>
 </div>
 <style>
-	
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.chartGrowth{
+		text-align: center;
+		float: center;
+		padding-top: 2rem;
 	}
-
+	.chartGrowth p{
+		font-size: 20px;
+	}
 </style>
--- src/data.js
+++ src/data.js
@@ -24,6 +24,10 @@ const initData = () => {
     addToData(data[data.length-1].id+1, [321.312,321352.23], "Heimo Heimola", new Date(2021,3), new Date(2021, 9), 1);
     addToData(data[data.length-1].id+1, [321.312,321353.23], "Heimo Heimola", new Date(2021,4), new Date(2022, 1), 2);
     addToData(data[data.length-1].id+1, [324.312,3232151.23], "Katri Keijola", new Date(2021,1), new Date(2021, 8), 2);
+    addToData(data[data.length-1].id+1, [321.312,321353.23], "Heimo Heimola", new Date(2021,4), new Date(2022, 1), 3);
+    addToData(data[data.length-1].id+1, [324.312,3232151.23], "Katri Keijola", new Date(2021,1), new Date(2021, 8), 3);
+    addToData(data[data.length-1].id+1, [321.312,321353.23], "Heimo Heimola", new Date(2021,4), new Date(2022, 1), 2);
+    addToData(data[data.length-1].id+1, [324.312,3232151.23], "Katri Keijola", new Date(2021,1), new Date(2021, 8), 3);
     return data;
 }
 export default initData;
