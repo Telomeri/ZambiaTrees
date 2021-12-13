4e0888d59edb39691121ace832e7f7e90f7da573
Lari-Haapaniemi <lari.haapaniemi@gmail.com>
2021-12-13 18:40:37 +0200

    Added a piechart sketch, new methods for updating data, counting up labels

--- src/DataScreen.svelte
+++ src/DataScreen.svelte
@@ -1,31 +1,101 @@
 <script>
 	import Chart from "svelte-frappe-charts";
+	import Counter from 'svelte-counter';
+import { update_keyed_each } from "svelte/internal";
 	export let data;
 	//show the amount planted per person?
 	//show the amount planted and checked per month? as a heatmap?
 	//show a scatter plot of the coordinates??
 	//show the amount of pictures uploaded? Maybe?
 	let growthAmount = [0,0,0,0];
-	data.forEach((x,i) => growthAmount[x.growth] += 1);
+	let planterAmount = {};
+	let updated = [0,0];
+	data.forEach((x,i) =>{
+		growthAmount[x.growth] += 1
+		if (x.planter in planterAmount) {
+			planterAmount[x.planter] += 1;
+		}
+		else {
+			planterAmount[x.planter] = 1;
+		}
+		var diff = new Date(x.updateDate.getTime() - x.plantDate.getTime());
+		if ((diff.getUTCFullYear() - 1970) >= 1) {
+			updated[1] += 1
+		} 
+		else {
+			updated[0] += 1
+		}
+	});
 	let growthData = {
 		labels : ["Seed","Sapling","Mature","Decline"],
 		datasets: [{
 			values: growthAmount
 		}]
-		
+	};
+	let updateData = {
+		labels : ["Updated","Outdated"],
+		datasets: [{
+			values: updated
+		}]
+	};
+	/*data = {
+    yMarkers: [
+        {
+            label: "Amount planted",
+            value: 4,
+            options: { labelPos: 'left' } // default: 'right'
+			}
+		]
+	}*/
+
+	let counters = {
+		'trees' : data.length,
+		'planters' : Object.keys(planterAmount).length
 	};
   </script>
-<div class="chartGrowth">
-	<p>Growth state</p>
-	<Chart data={growthData} type='percentage' colors={['#7F675B','#94FBAB','#058C42','#464e3c']} height={150} maxSlices = {5}/>
-</div>
+  <body>
+	<div class = "counterText">
+		<Counter values={counters} duration="2000" random="false" minspeed="10" let:counterResult>
+			<h1 class="trees">{counterResult.trees} Trees Planted</h1>
+			<h1 class="planter">{counterResult.planters} Volunteers Active</h1>
+		</Counter>
+	</div>
+	<div class="pieChart"><Chart data={updateData} type='pie' maxSlices={2} height={300} colors={['#94FBAB','#464e3c']} font-family = {"Lucida Console", "Courier New", 'monospace'}/></div>
+	<div class="chartGrowth">
+		<p>Growth state</p>
+		<Chart data={growthData} type='percentage' barOptions ={{height: 25, depth: 10}} colors={['#7F675B','#94FBAB','#058C42','#464e3c']} height={150} maxSlices = {5}/>
+	</div>
+</body>
 <style>
+	/*body {
+		background-color: #465a27;
+	}*/
 	.chartGrowth{
+		margin-left: 10%;
+		margin-right: 10%;
 		text-align: center;
 		float: center;
-		padding-top: 2rem;
 	}
+	.pieChart {
+		padding-left: 25%;
+		padding-right: 25%;
+		max-width: 50%;
+	} 
 	.chartGrowth p{
-		font-size: 20px;
+		font-size: 30px;
+	}
+	.counterText .trees{
+		margin: 0;
+		text-align: center;
+		padding-top: 3rem;
+		float: center;
+		font-size: 30px;
+		font-family:"Lucida Console", "Courier New", monospace;
+	}
+	.counterText .planter{
+		font-size: 25px;
+		float: center;
+		text-align: center;
+		font-family:"Lucida Console", "Courier New", monospace;
 	}
 </style>
--- src/data.js
+++ src/data.js
@@ -4,30 +4,48 @@ let data = [{
     planter: "Keijo Katala",
     plantDate: new Date(2020, 1),
     updateDate: new Date(2021, 1),
-    growth: 1
+    growth: 1,
+    pictures: ["here is the image?"]
   }];
 
-const addToData = (id, coordinates, planter, plantDate, updateDate, growth) => {
+const addData = (id, coordinates, planter, plantDate, updateDate, growth, pictures) => {
     data.push({
         id: id,
         coordinates: coordinates,
         planter: planter,
         plantDate: plantDate,
         updateDate:updateDate,
-        growth: growth
+        growth: growth,
+        pictures: pictures
       });
 }
 
+const updatePicture = (id, date, picture) => {
+  data.forEach((x,i) => {
+    if (x.id == id) {
+      data[i].updateDate = date;
+      data[i].pictures.push(picture);
+    }
+  });
+}
+
+
 const initData = () => {
     //addToData(int,list[float,float],"string", new Date(), new Date(), int)
-    addToData(data[data.length-1].id+1, [321.312,321351.23], "Heimo Heimola", new Date(2021,1), new Date(2021, 8), 0);
-    addToData(data[data.length-1].id+1, [321.312,321352.23], "Heimo Heimola", new Date(2021,3), new Date(2021, 9), 1);
-    addToData(data[data.length-1].id+1, [321.312,321353.23], "Heimo Heimola", new Date(2021,4), new Date(2022, 1), 2);
-    addToData(data[data.length-1].id+1, [324.312,3232151.23], "Katri Keijola", new Date(2021,1), new Date(2021, 8), 2);
-    addToData(data[data.length-1].id+1, [321.312,321353.23], "Heimo Heimola", new Date(2021,4), new Date(2022, 1), 3);
-    addToData(data[data.length-1].id+1, [324.312,3232151.23], "Katri Keijola", new Date(2021,1), new Date(2021, 8), 3);
-    addToData(data[data.length-1].id+1, [321.312,321353.23], "Heimo Heimola", new Date(2021,4), new Date(2022, 1), 2);
-    addToData(data[data.length-1].id+1, [324.312,3232151.23], "Katri Keijola", new Date(2021,1), new Date(2021, 8), 3);
+    //0
+    addData(data[data.length-1].id+1, [321.312,321351.23], "Heimo Heimola", new Date(2021,1), new Date(2021, 8), 0, ["here is the image?"]);
+    addData(data[data.length-1].id+1, [321.312,321352.23], "Heimo Heimola", new Date(2021,3), new Date(2021, 9), 1, ["here is the image?"]);
+    addData(data[data.length-1].id+1, [321.312,321353.23], "Heimo Heimola", new Date(2021,4), new Date(2022, 1), 2,["here is the image?"]);
+    //3
+    addData(data[data.length-1].id+1, [324.312,3232151.23], "Katri Keijola", new Date(2021,1), new Date(2021, 8), 2,["here is the image?"] );
+    addData(data[data.length-1].id+1, [321.312,321353.23], "Heimo Heimola", new Date(2021,4), new Date(2022, 1), 3,["here is the image?"]);
+    addData(data[data.length-1].id+1, [324.312,3232151.23], "Katri Keijola", new Date(2021,1), new Date(2021, 8), 3,["here is the image?"]);
+    //6
+    addData(data[data.length-1].id+1, [321.312,321353.23], "Heimo Heimola", new Date(2021,4), new Date(2022, 1), 2,["here is the image?"]);
+    addData(data[data.length-1].id+1, [324.312,3232151.23], "Katri Keijola", new Date(2021,1), new Date(2021, 8), 3,["here is the image?"]);
+    updatePicture(3,new Date(2021,12),"there is an update!");
+    updatePicture(5,new Date(2021,12),"there is an update!");
+    updatePicture(6,new Date(2021,12),"there is an update!");
     return data;
 }
 export default initData;
