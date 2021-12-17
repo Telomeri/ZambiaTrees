432cba20181b3b4568806b715171707db5c06729
Tomas <tomas.hyvonen@gmail.com>
2021-12-17 15:18:20 +0200

    Display map and markers

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,46 @@
 <script>
-	export let name;
+    export let name;
+    import Map from "./Map/Map.svelte";
+    import MapMarker from "./Map/MapMarker.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="overlay">
+    <Map lat={-15.908235} lon={25.957893} zoom={15}>
+        <MapMarker lat={-15.908235} lon={25.957893} label="Tree 1" />
+        <MapMarker
+            lat={-15.908235}
+            lon={25.96}
+            label="Tree 2"
+        />
+    </Map>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+
+    .overlay {
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+    }
 </style>
---
+++ src/Map/mapbox.js
@@ -0,0 +1,8 @@
+import mapbox from 'mapbox-gl';
+
+// https://docs.mapbox.com/help/glossary/access-token/
+mapbox.accessToken = 'pk.eyJ1IjoidGh5dm9uZW4iLCJhIjoiY2tjYnN3NW5tMjd6NjJ2bWdscG1vNHNoeSJ9.OAMTF0xUTjnyRFdJVzJy5g';
+
+const key = {};
+
+export { mapbox, key };
