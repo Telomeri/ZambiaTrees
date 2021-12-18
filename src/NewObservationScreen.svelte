9141393f996dd6a2b171c75b338b6ef2c8965f3a
Panu Liesiö <panu.liesio@aalto.fi>
2021-12-18 21:29:49 +0200

    add confirmation buttons to observations

--- src/NewObservationScreen.svelte
+++ src/NewObservationScreen.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Container, Row, Icon, Figure, Image } from 'sveltestrap';
+  import { Container, Row, Col, Icon, Figure, Image } from 'sveltestrap';
 	import { useNavigate } from "svelte-navigator";
 
 	const navigate = useNavigate();
@@ -19,17 +19,27 @@
       <img class="img-fill" src={getImageSrc('sus')} />
     </div>
   </Row>
-  <Row cols={{xs: 1, sm: 2, lg: 4}}>
+  <Row>
+    <Col class="pt-3">
+      Can you recognize this image from your surroundings?
+    </Col>
+  </Row>
+  <Row class="pt-3">
+    <Col>
+      <button>
+        no
+      </button> 
+    </Col>
+    <Col>
+      <button>
+        yes
+      </button>  
+    </Col>
   </Row>
 </Container>
 
 
 <style>
-  .asdfg {
-    min-height: 80px;
-    font-size: 1.3rem;
-  }
-
   .img-container {
     max-height: 60vh;
   }
@@ -45,10 +55,6 @@
 		background-color: #5CDB95 !important;
 	}
 
-  .welcome-message {
-    color: #05386B;
-  }
-
   button {
     background-color: #EDF5E1;
     border-radius: 1rem;
@@ -58,18 +64,23 @@
     height: 100%;
     box-sizing: border-box;
     border: none;
-    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-    transition: all 0.3s ease 0s;
+    text-align: center;
+    /* box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1); */
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+    /* transition: all 0.3s ease 0s; */
+  }
+
+  button:active {
+    /* background-color: #379683;; */
+    /* box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.1); */
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 7px 13px -3px inset, rgba(0, 0, 0, 0.2) 0px 3px 0px inset; 
+    background-color: #C7CFBB;
+    /* transform: translateY(3px); */
   }
 
   button:hover {
-    background-color: #379683;;
-    box-shadow: 0px 15px 20px rgba(0, 0, 0, 0.1);
-    color: #EDF5E1;
-    transform: translateY(-3px);
-  }
-
-  button > span {
-    align-self: center;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+    background-color: #FAFFEE;
+    transform: translateY(-2px);
   }
 </style>
