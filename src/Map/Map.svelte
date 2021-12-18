357922ae098b8621ac6478adf47f4f1105e61883
Tomas <tomas.hyvonen@gmail.com>
2021-12-18 13:59:48 +0200

    Fix map display in main

--- src/Map/Map.svelte
+++ src/Map/Map.svelte
@@ -5,6 +5,7 @@
     import { Button } from "sveltestrap";
     import { useNavigate } from "svelte-navigator";
     import MapMarker from "./MapMarker.svelte";
+    import Navbar from "../Navbar.svelte"
 
     const navigate = useNavigate();
     let mapComponent;
@@ -26,20 +27,33 @@
     }
 </script>
 
-<Map
-    accessToken="pk.eyJ1IjoidGh5dm9uZW4iLCJhIjoiY2tjYnN3NW5tMjd6NjJ2bWdscG1vNHNoeSJ9.OAMTF0xUTjnyRFdJVzJy5g"
-    style="mapbox://styles/mapbox/satellite-v9"
-    center={[lon, lat]}
-    {zoom}
-    bind:this={mapComponent}
-    on:ready={onReady}
->
-    <!-- <div class="mapboxgl-ctrl-bottom-right">
+<div class="fullscreen">
+    <Navbar barText="Map" />
+    <Map
+        accessToken="pk.eyJ1IjoidGh5dm9uZW4iLCJhIjoiY2tjYnN3NW5tMjd6NjJ2bWdscG1vNHNoeSJ9.OAMTF0xUTjnyRFdJVzJy5g"
+        style="mapbox://styles/mapbox/satellite-v9"
+        center={[lon, lat]}
+        {zoom}
+        bind:this={mapComponent}
+        on:ready={onReady}
+    >
+        <!-- <div class="mapboxgl-ctrl-bottom-right">
         <Button>ASD</Button>
     </div> -->
-    {#if data}
-        {#each data as tree}
-            <MapMarker tree={tree} />
-        {/each}
-    {/if}
-</Map>
+        {#if data}
+            {#each data as tree}
+                <MapMarker {tree} />
+            {/each}
+        {/if}
+    </Map>
+</div>
+
+<style>
+    .fullscreen {
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+    }
+</style>
